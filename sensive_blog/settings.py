6a7f296e4f3d57725e7597d41ca5840e5e9f45ea
Faezragore <30468128+Faezragore@users.noreply.github.com>
2020-11-03 00:45:52 +0300

    Update settings.py

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -93,9 +93,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = os.getenv('STATIC_URL', '/static/')
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = ["./static/",
+     "./static/fonts/",
+     "./static/img/",
+     "./static/",]
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
